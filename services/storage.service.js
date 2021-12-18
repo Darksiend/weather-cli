99129ac4fc50b9cfd76dd98e7b89a77014baabdf
Darksiend <logosmk2@gmail.com>
2021-12-18 16:49:53 +0200

    Main

---
+++ services/storage.service.js
@@ -0,0 +1,27 @@
+import { homedir } from 'os';
+import { join } from 'path';
+import { promises } from 'fs';
+
+const filePath = join(homedir(), 'weather-data.json');
+
+const saveKeyValue = async(key, value) => {
+    const data = {};
+    if (await isExist(filePath)) {
+        const file = await promises.readFile(filePath);
+        data = JSON.parse(file);
+    }
+    data[key] = value;
+    await promises.writeFile(filePath, JSON.stringify(data))
+};
+
+const isExist = async (path) => {
+    try {
+        await promises.stat(path);
+        return true;
+    } catch (e) {
+        return false;
+    }
+    
+}
+
+export { saveKeyValue };
