0c687ff5d7f494ba836f2d5540a06188a7b5b6eb
Darksiend <logosmk2@gmail.com>
2021-12-28 14:16:52 +0200

    Update weather.js

--- weather.js
+++ weather.js
@@ -19,8 +19,19 @@ const saveToken = async (token) => {
 }
 
 const getForecast = async () => {
-    const weather = await getWeather('moscow');
-    console.log(weather);
+    try {
+        const weather = await getWeather(process.env.CITY);
+        console.log(weather);
+    } catch (e) {
+        if (e?.response?.status == 404) {
+            printError("Неверно указан город");
+        }
+        else if (e?.response?.status == 401) {
+            printError("Неверно указан токен");
+        } else {
+            printError(e.message);
+        }
+    }
 }
 
 const initCLI = () => {
